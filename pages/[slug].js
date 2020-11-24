4fc3caeb82c05b167721744d52fe6965c596abe0
Tim Mortimer <tim@kiteframe.co.uk>
2020-11-24 18:04:36 +0000

    Render markdown as HTML

---
+++ pages/[slug].js
@@ -0,0 +1,37 @@
+import {getAllPosts, getPostBySlug} from '../lib/posts';
+import markdownToHtml from '../lib/markdown';
+
+export default function Post({meta, content}) {
+    return (
+        <div className="container mx-auto">
+            <article className="prose" dangerouslySetInnerHTML={{__html: content}}/>
+        </div>
+    );
+}
+
+export async function getStaticProps({params}) {
+    const post = getPostBySlug(params.slug);
+    const content = await markdownToHtml(post.content || '');
+
+    return {
+        props: {
+            ...post,
+            content
+        }
+    };
+}
+
+export async function getStaticPaths() {
+    const posts = getAllPosts();
+
+    return {
+        paths: posts.map((post) => {
+            return {
+                params: {
+                    slug: post.slug
+                }
+            };
+        }),
+        fallback: false
+    };
+}

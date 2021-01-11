4772f24a58683c729b4e39a021af8e77c0112ae1
Tim Mortimer <tim@kiteframe.co.uk>
2021-01-11 12:01:58 +0000

    Apply Prettier

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,30 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <title/>
-                    <script async defer data-domain="kiteframe.co.uk" src="https://plausible.io/js/plausible.outbound-links.js"/>
-                </Head>
-                <body>
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title />
+          <script
+            async
+            defer
+            data-domain="kiteframe.co.uk"
+            src="https://plausible.io/js/plausible.outbound-links.js"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
